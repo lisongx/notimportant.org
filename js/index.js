3c0091d550835efb1dd7ffa9e2d0bcb9f408574c
Sean Lee <iseansay@gmail.com>
2014-01-14 20:30:46 +0800

    background audio

---
+++ js/index.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function() {
+  var cc = new CoffeeCollider();
+  var code = "(-> SinOsc.ar(SinOsc.ar(8, 0, 10, MouseX.kr(400, 1000, 1)))).play()";
+  var btn = $("#audioBtn");
+  var PLAY_ICON_CLASS = "fa-play";
+  var PAUSE_ICON_CLASS= "fa-pause";
+
+  btn.on("click", function(){
+    if($(this).hasClass(PLAY_ICON_CLASS)) {
+      cc.run(code);
+      $(this).removeClass(PLAY_ICON_CLASS);
+      $(this).addClass(PAUSE_ICON_CLASS);
+    } else {
+      cc.pause();
+      $(this).removeClass(PAUSE_ICON_CLASS);
+      $(this).addClass(PLAY_ICON_CLASS);
+    }
+  });
+});
+
+
+
+
